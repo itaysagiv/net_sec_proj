fad6c6d24ad7253f109568e991fc3a168643e5b4
Mark Lechtik <marklech90@gmail.com>
2017-07-16 16:31:00 +0300

    restore

--- run_receiver.sh
+++ run_receiver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-IP="132.73.202.38" #ip for input from the encryptor
+IP="192.168.10.103" #ip for input from the encryptor
 PORT="5555" #port for input from the encryptor
 PROTO="udp"
 
 clear
 
-xterm -e vlc $PROTO://@$IP:$PORT 
+xterm -e vlc $PROTO://@127.0.0.1:8888 &
 
-#xterm -e python decryption_server.py $IP $PORT 127.0.0.1 8888
+xterm -e python decryption_server.py $IP $PORT 127.0.0.1 8888
