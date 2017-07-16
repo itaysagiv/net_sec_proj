c89ce38a3842ce4632673245f29849cf0bb5360f
Mark Lechtik <marklech90@gmail.com>
2017-07-16 16:01:14 +0300

    test

--- run_receiver.sh
+++ run_receiver.sh
@@ -6,6 +6,6 @@ PROTO="udp"
 
 clear
 
-xterm -e vlc $PROTO://@127.0.0.1:8888 &
+xterm -e vlc $PROTO://@$IP:$PORT 
 
-xterm -e python decryption_server.py $IP $PORT 127.0.0.1 8888
+#xterm -e python decryption_server.py $IP $PORT 127.0.0.1 8888
--- run_transmiter.sh
+++ run_transmiter.sh
@@ -4,11 +4,11 @@ IP="132.73.202.38" #ip for out to decryption
 PORT="5555" #port for out to decryption
 
 PROTO="udp"
-VIDEO_FILE="b.mp4"
+VIDEO_FILE="a.mp4"
 VIDEO_TIME="2:00"
 
 clear
 
-xterm -hold -e python encryption_server.py 127.0.0.1 1234 $IP $PORT &
+#xterm -hold -e python encryption_server.py 127.0.0.1 1234 $IP $PORT &
 
-xterm -e ffmpeg -re  -i $VIDEO_FILE -c copy -ss 00:00:00  -t $VIDEO_TIME  -bsf:v h264_mp4toannexb -f mpegts $PROTO://127.0.0.1:1234
+xterm -e ffmpeg -re  -i $VIDEO_FILE -c copy -ss 00:00:00  -t $VIDEO_TIME  -bsf:v h264_mp4toannexb -f mpegts $PROTO://$IP:$PORT
